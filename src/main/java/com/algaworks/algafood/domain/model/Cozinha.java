7b5926a3995626958b8284a50003b843d19f59be
Rafael Modesto <rafaelmodesto09@gmail.com>
2022-06-07 18:57:59 -0300

    Implementado consulta singleton resource com GET e @PathVariable, inclusao de recurso com POST, atualizacao de recursos com PUT, deletando recurso com DELETE, manipulacao de respostas HTTP com ResponseEntity, iniciando camada de domain service(DDD)

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
