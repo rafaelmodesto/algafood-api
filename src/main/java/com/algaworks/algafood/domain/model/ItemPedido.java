bafbe796ed0bf8f4f579c5acbeca9151370b1cca
Rafael Modesto <rafaelmodesto09@gmail.com>
2022-06-29 18:03:13 -0300

    criando entidades pedido, itempedido e suas migrations

---
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class ItemPedido {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantidade;
+    private BigDecimal precoUnitario;
+    private BigDecimal precoTotal;
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Pedido pedido_id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Produto produto;
+}
---
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -0,0 +1,55 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class Pedido {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal subtotal;
+    private BigDecimal taxaFrete;
+    private BigDecimal valorTotal;
+
+    @Embedded
+    private Endereco enderecoEntrega;
+
+    private StatusPedido status;
+
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    private LocalDateTime dataConfirmacao;
+    private LocalDateTime dataCancelamento;
+    private LocalDateTime dataEntrega;
+
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private FormaPagamento formaPagamento;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurante restaurante;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Usuario cliente;
+
+    @OneToMany(mappedBy = "pedido_id")
+    private List<ItemPedido> itens = new ArrayList<>();
+
+}
